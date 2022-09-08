43a46bebcef3462380a084ffaf83d9b51ee578e0
suneela1823 <113087556+suneela1823@users.noreply.github.com>
2022-09-08 11:43:53 +0530

    Add files via upload

---
+++ practical1.js
@@ -0,0 +1,14 @@
+console.log("welcome to Node Js");
+vara=6;
+varb=8;
+var c = a + b;
+console.log(`A value is ${a} and B value is ${b});
+sum is $(a+b)`);
+
+if(c==30)
+{
+    console.log("sum is 30");
+}
+else{
+    console.log("Else");
+}
